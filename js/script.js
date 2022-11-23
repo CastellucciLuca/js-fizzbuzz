5a35bf87ab70a257cef9c2a614eb801a69f31781
CastellucciLuca <castellucciluca98@gmail.com>
2022-11-23 15:17:17 +0100

    first-commit-add-fizz-content

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Scrivi un programma che stampi in console i numeri da 1 a 100:
+//Per i multipli di 5 stampi “Buzz”.
+//Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+//Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+//Let
+let fizz = document.getElementById("fizz")
+
+let buzz = document.getElementById("buzz")
+
+let fizzBuzz = document.getElementById("fizzBuzz")
+
+let fizzNumber = document.getElementById("fizzNumber")
