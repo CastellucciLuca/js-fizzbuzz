ab8b6430f9b5c951cd5b6a5492cc190f3c89f230
CastellucciLuca <castellucciluca98@gmail.com>
2022-11-23 15:26:19 +0100

    js-if-else

--- js/script.js
+++ js/script.js
@@ -11,3 +11,19 @@ let buzz = document.getElementById("buzz")
 let fizzBuzz = document.getElementById("fizzBuzz")
 
 let fizzNumber = document.getElementById("fizzNumber")
+
+//FIRST IF Numbers that are multiples of 3 and 5
+//FIRST ELSE IF multiples of 5
+//SECOND ELSE IF multiples of 3
+for (let i = 0; i <= 100; i++) {
+    if (i % 15 === 0){
+        console.log("fizzBuzz")
+    } else if (i % 5 === 0) {
+        console.log("buzz")
+    } else if (i % 3 === 0) {
+        console.log("fizz")
+    } else {
+        console.log(i)
+    }
+
+}
